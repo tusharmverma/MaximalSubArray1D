67e2f3c02f9b376e4071a569cfad85c158ebbda1
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 09:09:34 -0500

    Max Sub Array 2D Function Added

--- maxSubArray1D.cpp
+++ maxSubArray1D.cpp
@@ -35,3 +35,48 @@ void MaxSumSubArray( int a[], int size)
     cout << "Max Sum Start : " << MaxSumStart << endl ;
     cout << "Max Sum End : " << MaxSumEnd-1 << endl;
 }
+
+/**
+ * Method: MaxSumSubArray2D
+ *    Function to find the Maximum sub-array of given 2D Array.
+ *
+ * @param   a[][COL] - The 2D Array Matrix of integers
+ *
+ * @return  nothing
+ */
+void MaxSumSubArray2D( int **a, int rows, int columns)
+{
+    int MaxSum = -1;
+    int MaxSumTop = 0;
+    int MaxSumBottom = 0;
+    int MaxSumLeft = 0;
+    int MaxSumRight = 0;
+    for( int i = 0; i < rows; i++)
+    {
+        for(int j = i; j < columns ; j++)
+        {
+            for(int p = 0;p < rows;p++){
+                
+                for(int q = p; q < columns; q++){
+                    int tempSum = 0;
+                    for(int r = i ; r < j ; r++){
+                        for (int k = p; k < q; k++)
+                            tempSum += a[r][k];
+                        
+                        if (MaxSum < tempSum){
+                            MaxSum = tempSum;
+                            MaxSumTop = p;
+                            MaxSumBottom = q-1;
+                            MaxSumLeft = i;
+                            MaxSumRight = j-1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << "Max Sum in the array : " << MaxSum << endl;
+    cout << "Max Sum (Top,left) : (" << MaxSumTop << " , " << MaxSumLeft << ")" ;
+    cout << "\nMax Sum (Bottom,right) : (" << MaxSumBottom << " , " << MaxSumRight << ")" << endl;
+}
+
